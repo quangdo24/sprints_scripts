1bcbe01a6764dc1862cd94f9741d2899824d0954
quangdo24 <qd24@live.com>
2026-04-29 06:37:46 +0000

    Set up Environment and .py file for sprint2

---
+++ sysinfo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Description of this script goes here
+# Licensed under the MIT License (https://opensource.org/license/mit)
+# QuangDo-20260426: V1.0.0
+
+# Set up initial variables and imports
+import os
+import platform
+
+# Script/Library Functions
+def main():
+    """Main function to parse the log file and write the results to a CSV file."""
+   
+
+
+
+            
+# Run main() if script called directly, else use as a library to be imported
+if __name__ == '__main__':
+    main()
